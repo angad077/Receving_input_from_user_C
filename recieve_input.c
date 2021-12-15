5d3773c4e25d7ccbdef5d6bd4eab5d537a8c757c
angad <angadreact01@gmail.com>
2021-12-15 09:00:51 -0800

    first commit

---
+++ recieve_input.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    printf("Enter your a value\n");
+    scanf("%d", &a);
+    printf("Enter your b value\n");
+    scanf("%d", &b);
+    printf("your total is %d \n", a + b);
+    return 0;
+}
